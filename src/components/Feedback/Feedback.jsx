c520fe733f8ad7bce701f4baae066d81a748d93c
rvzdnk <rafal93zdunek@gmail.com>
2022-08-20 08:14:07 +0200

    step 4

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Statistics from "components/Statistics/Statistics";
 import Section from "components/Section/Section";
+import Notification from "components/Notification/Notification";
 
 class Feedback extends React.Component{
     state = {
@@ -24,23 +25,34 @@ class Feedback extends React.Component{
         return  this.countTotalFeedback() > 0 ? ((this.state.good/this.countTotalFeedback())*100).toFixed(2) : 0;
     };
 
+    rednerStatisticsSection =() => {
+        if (this.countTotalFeedback() === 0){
+            return <Notification message="There is no feedback"/>;
+        }
+        return (
+        <Section title ="Statistics">
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback}
+                positivePercentage={this.countPositiveFeedback}/>
+        </Section>
+        )
+    };
+
     render() {
         const {good, neutral, bad} = this.state;
         const options = Object.keys(this.state);
 
         return(
             <div>
-            <Section title ="Please leave feedback"/>
+            <Section title ="Please leave feedback">
                 <FeedbackOptions
                     options={options}
                     onLeaveFeedback={this.incraseStateField}/>
-            <Section title ="Statistics"/>
-                <Statistics
-                    good={good}
-                    neutral={neutral}
-                    bad={bad}
-                    total={this.countTotalFeedback}
-                    positivePercentage={this.countPositiveFeedback}/>
+            </Section>
+            {this.rednerStatisticsSection()}
             </div>
         )
     }
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Notification extends React.Component{
+    render() {
+        const { message } = this.props;
+        return
+        <p>
+            {message || ''}
+        </p>;
+    }
+};
+
+export default Notification;
